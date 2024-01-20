7e576cb8aff0c693e4c9d5d37f526bcda78f8a89
sachinio99 <sachinshah0099@gmail.com>
2024-01-20 18:03:25 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,10 @@
 import React from 'react';
-import { ChakraProvider } from '@chakra-ui/react';
 import RootLayout from './RootLayout';
 
 export default function Page() {
   return (
-    <ChakraProvider>
-      <RootLayout>
-        <h1>Hello, Next.js!</h1>
-      </RootLayout>
-    </ChakraProvider>
+    <RootLayout>
+      <h1>Hello, Next.js!</h1>
+    </RootLayout>
   );
 }
