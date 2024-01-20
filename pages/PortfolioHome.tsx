b23967a2f6ed239497369e81a580e2d3261b933e
sachinio99 <sachinshah0099@gmail.com>
2024-01-20 18:25:12 -0500

    adding pages

---
+++ pages/PortfolioHome.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const PortfolioHome = () => {
+  return (
+    <div>
+      <h1>Portfolio</h1>
+      {/* Add your portfolio content here */}
+    </div>
+  );
+};
+
+export default PortfolioHome;
